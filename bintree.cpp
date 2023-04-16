d914042863b3d181dfb7b54a9f16513c17ffd944
Chris <cbzoozoom7@gmail.com>
2023-04-16 12:05:42 -0500

    wrote the BinTree class prototype & function stubs.

--- bintree.cpp
+++ bintree.cpp
@@ -2,5 +2,75 @@
 //Chris Bowman
 //Created 16 Apr 2023
 #include "bintree.h"
+BinTree::BinTree() {
 
+}
+BinTree::~BinTree() {
 
+}
+bool BinTree::isEmpty() {
+    return false;
+}
+int BinTree::getCount() {
+    return -1;
+}
+bool BinTree::getRootData(Data *d) {
+    return false;
+}
+void BinTree::displayTree() {
+
+}
+void BinTree::clear() {
+
+}
+bool BinTree::addNode(int id, const string *data) {
+    return false;
+}
+bool BinTree::removeNode(int id) {
+    return false;
+}
+bool BinTree::getNode(Data* d, int id) {
+    return false;
+}
+bool BinTree::contains(int id) {
+    return false;
+}
+int BinTree::getHeight() {
+    return -1;
+}
+void BinTree::displayPreOrder() {
+
+}
+void BinTree::displayPostOrder() {
+
+}
+void BinTree::displayInOrder() {
+
+}
+void BinTree::clear(DataNode *tempRoot) {
+
+}
+bool BinTree::addNode(DataNode *data, DataNode **tempRoot) {
+    return false;
+}
+DataNode *BinTree::removeNode(int id, DataNode *tempRoot) {
+    return nullptr;
+}
+bool BinTree::getNode(Data *d, int id, DataNode *tempRoot) {
+    return false;
+}
+bool BinTree::contains(int id, DataNode *tempRoot) {
+    return false;
+}
+int BinTree::getHeight(DataNode *tempRoot) {
+    return -1;
+}
+void BinTree::displayPreOrder(DataNode *tempRoot) {
+
+}
+void BinTree::displayPostOrder(DataNode *tempRoot) {
+
+}
+void BinTree::displayInOrder(DataNode *tempRoot) {
+
+}
